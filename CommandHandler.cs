f5186a00615021d846f2bf6bdb5edfddaba70e9f
scovee <30730053+scovee@users.noreply.github.com>
2019-05-01 00:40:59 +0800

    added core
    gamer mode

---
+++ CommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace KumikoV4
+{
+    class CommandHandler
+    {
+        private DiscordSocketClient _client;
+        private CommandService _service;
+
+        public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+            _service = new CommandService();
+            _service.AddModulesAsync(Assembly.GetEntryAssembly());
+            _client.MessageReceived += CommandHandlerAsync;
+        }                                                            
+
+        public async Task CommandHandlerAsync(SocketMessage arg)
+        {
+            var msg = arg as SocketUserMessage;
+            if (msg == null) return;
+            var context = new SocketCommandContext(_client, msg);
+            int argPos = 0;
+            if (msg.HasStringPrefix("!k ", ref argPos))
+            {
+                var result = await _service.ExecuteAsync(context, argPos);
+                if (!result.IsSuccess && result.ErrorReason == "Unknown command.")
+                {
+                    await context.Channel.SendMessageAsync("uWu onii-chan, I don't think I understand your `syntax` >.<" + System.Environment.NewLine + "(´；Д；`)");
+                }
+                if (!result.IsSuccess && result.ErrorReason == "The input text has too few parameters.")
+                {
+                    await context.Channel.SendMessageAsync(">.< onii-chan kono baka, i need you to f-fill me up with your `parameters`" + System.Environment.NewLine + "(〃ω〃)");
+                }
+                if (!result.IsSuccess && result.ErrorReason == "The input text has too many parameters.")
+                {
+                    await context.Channel.SendMessageAsync("onii-chan ga ecchi! how can I possibly fit that `parameter` inside me" + System.Environment.NewLine + "(｀・ω・´)");
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Discord.WebSocket;
+using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+using KumikoV4.WebAPI;
+
+namespace KumikoV4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+       => new Program().StartAsync().GetAwaiter().GetResult();
+        private DiscordSocketClient _client;
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig { LogLevel = LogSeverity.Verbose });
+            new CommandHandler(_client);
+            await _client.LoginAsync(Discord.TokenType.Bot, "NTU2MTI4MjcyOTU0NTU2NDE2.XL7aCg.6r0zN5OOSBbbCQbsJuAPL3mn1Q0");
+            await _client.StartAsync();
+            await Task.Delay(-1);
+            
+        }
+    }
+}
